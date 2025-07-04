f531c38bab3d7a790d1084e0d695514c2dbb923f
apridanhm <sagiriizumi728@gmail.com.com>
2025-07-04 02:23:55 +0000

    coba coba day 2

--- rust_day1v2/src/main.rs
+++ rust_day1v2/src/main.rs
@@ -55,10 +55,70 @@ fn hitung_kelipatan(x: u32) {
     }
 }
 
+fn cocok() {
+    let aku = 12;
+    match aku {
+        1 => println!("saya"),
+        12 => println!("saya {}", aku),
+        _ => println!("noting!!")
+    } 
+}
+
+fn apalah() {
+
+    let mut i = 1;
+    while i <= 99 {
+        println!("ini ke-{}", i);
+
+        i += 3;
+    }
+}
+
+fn nilai_match() {
+    let nilai = 90;
+    
+    match nilai {
+        1..=5 => println!("bodoh"),
+        6..=7 => println!("lumayan"),
+        8..=10 => println!("amajing"),
+        n if n > 10 => println!("amajing+++"),
+        _ => println!("noting!!")
+    }
+}
+
+fn ssss() {
+    let angka = 214748364;
+
+    match angka {
+        kecil @ 0..=7 => println!("kecil ({})", kecil),
+        sedang @ 8..=10 => println!("amajing ({})", sedang),
+        besar @ 11..=i32::MAX => println!("good ({})", besar),
+        _ => println!("nilai tak terduga"),
+    }
+}
+
+// tuple 
+
+fn alia() {
+
+    let profile = ("aligator", 222, 321);
+
+    println!("nama : {}", profile.0);
+    println!("umur : {}", profile.1);
+    println!("tinggi : {}", profile.2);
+}
+
+
+
 fn main() {
     tampilkan_profil("Rustacean", 28);
     println!();
     hitung_kelipatan(7);
+    cocok();
+    apalah();
+    nilai_match();
+    ssss();
+    alia();
 }
 
 
